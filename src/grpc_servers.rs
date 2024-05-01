d6b2b675638385108105d3709ecdee7f5734e07e
Maulana Seto <maulana.seto@ui.ac.id>
2024-05-01 14:42:42 +0700

    Payment Service Implementation

---
+++ src/grpc_servers.rs
@@ -0,0 +1,33 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod services {
+    tonic::include_proto!("service")
+}
+
+use services::{payment_service_server::{PaymentService, PaymentServiceServer}, PaymentRequest, PaymentResponse};
+
+#[derive(Default)]
+pub struct MyPaymentService {}
+
+#[tonic::async_trait]
+impl PaymentService for MyPaymentService {
+    async fn process_payment(
+        $self,
+        request: Request<PaymentRequest>,
+    ) -> Result<Response<PaymentResponse>, Status> {
+        println!("Received payment request: {:?}", request);
+        Ok(Response::new(PaymentResponse {success: true}))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "{::1}:50051".parse()?;
+    let payment_service = MyPaymentService::default();
+
+    Server::builder()
+        .add_service(PaymentServiceCenter::new(payment_servicet))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
